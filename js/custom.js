365482a87e5ccdfff9aa0f162f5779fdb9d88eb7
TTTRocketer <tsathit40@hotmail.com>
2022-12-12 13:30:02 +0700

    Level 2

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,20 @@
 ﻿
+function openFullscreen() {
+    var el = document.documentElement
+        , rfs =
+            el.requestFullScreen
+            || el.webkitRequestFullScreen
+            || el.mozRequestFullScreen
+        ;
+    rfs.call(el);
+}
+
+function reportSize() {
+    var html = $(window).width() + ' x ' + $(window).height();
+    $('.size-helper').html(html);
+}
+
+$(window).resize(function () {
+    reportSize();
+});
+
